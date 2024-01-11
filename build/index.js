832deec33198d3411b010ca87f0190a31583c71f
Robert Lester <robertlesterjr@mac.com>
2024-01-11 13:55:51 -0500

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,13 @@
+/*!
+ * 
+ *   @hodgef/js-library-boilerplate v2.7.279
+ *   https://github.com/hodgef/js-library-boilerplate
+ *
+ *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
+ *
+ *   This source code is licensed under the MIT license found in the
+ *   LICENSE file in the root directory of this source tree.
+ *
+ */
+!function(e,o){"object"===typeof exports&&"object"===typeof module?module.exports=o():"function"===typeof define&&define.amd?define("MyLibrary",[],o):"object"===typeof exports?exports.MyLibrary=o():e.MyLibrary=o()}(this,(()=>(()=>{"use strict";var e={d:(o,t)=>{for(var r in t)e.o(t,r)&&!e.o(o,r)&&Object.defineProperty(o,r,{enumerable:!0,get:t[r]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o),r:e=>{"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},o={};e.r(o),e.d(o,{default:()=>t});const t=class{constructor(){this.myVar=!0,this.myArrowMethod=()=>{console.log("Arrow method fired")};const{myArrowMethod:e,myVar:o}=this;console.log("Lib constructor called",o),e()}};return o})()));
+//# sourceMappingURL=index.js.map
