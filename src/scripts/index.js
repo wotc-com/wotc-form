9a807e40a95704670a773df2a2ac398419886b94
Robert Lester <rlester@tcsvc.com>
2024-01-16 12:02:45 -0500

    finally got the basics working. Now need to grab fields from wotc apis.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,37 @@
 import '../styles/index.scss';
+import $ from 'jquery';
+import { createRoot } from 'react-dom/client';
 
-console.log('webpack starterkit');
+import React, { useState } from 'react';
+import { JsonForms } from '@jsonforms/react';
+
+import { person } from '@jsonforms/examples';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+
+window.$ = $;
+
+const schema = person.schema;
+const uischema = person.uischema;
+const initialData = person.data;
+
+function App() {
+  const [data, setData] = useState(initialData);
+  return (
+	<div className='App'>
+	  <JsonForms
+		schema={schema}
+		uischema={uischema}
+		data={data}
+		renderers={materialRenderers}
+		cells={materialCells}
+		onChange={({ data, errors }) => setData(data)}
+	  />
+	</div>
+  );
+}
+
+const root = createRoot(document.getElementById('wotcForm'));
+root.render(<App />);
